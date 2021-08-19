ec32ac30411430b9627254520d010730ee41435f
sudo-rushil <rushil.mallarapu@gmail.com>
2021-08-19 01:44:54 -0400

    mildly edited snippets and wrote guide

--- epistemic.sh
+++ epistemic.sh
@@ -7,6 +7,11 @@ set_course () {
 	ln -s $1 ~/Documents/Harvard/Classes/current
 }
 
+rm_course () {
+	echo "Clearing course..."
+	rm ~/Documents/Harvard/Classes/current 2> /dev/null
+}
+
 case $1 in
 	55)
 		set_course ~/Projects/Live/notes/snippets/Math55
@@ -17,9 +22,14 @@ case $1 in
 	1011)
 		set_course ~/Projects/Live/notes/snippets/Econ1011
 		;;
+	rm)
+		rm_course
+		;;
 	clean)
 		# Run *IN* directory!
-		latexmk -c
+		echo "Cleaning build files..."
+		rm -f *synctex*
+		latexmk -c &> /dev/null
 		;;
 	*)
 		echo "Unknown course: $1... exiting"
