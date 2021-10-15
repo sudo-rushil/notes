a1edca1a34d010b3170eb9d4efd7af1fde5c948a
sudo-rushil <rushil.mallarapu@gmail.com>
2021-10-15 17:11:18 -0400

    misc updates

--- epistemic.sh
+++ epistemic.sh
@@ -18,36 +18,41 @@ Usage: ep (55|110|1011|rm|clean)
 
 Commands:
 
-  - 55 			Set "current" to Math55
-  - 110			Set "current" to Stat110
-  - 1011		Set "current" to Econ1011
+  - ma 			Set "current" to Math55
+  - st			Set "current" to Stat110
+  - ec			Set "current" to Econ1011
   - rm			Remove "current"
   - clean		Clean current directory of LaTeX build files
 HELP
 }
 
-case $1 in
-	55)
-		set_course ~/Projects/Live/notes/snippets/Math55
-		;;
-	110)
-		set_course ~/Projects/Live/notes/snippets/Stat110
-		;;
-	1011)
-		set_course ~/Projects/Live/notes/snippets/Econ1011
-		;;
-	rm)
-		rm_course
-		;;
-	clean)
-		# Run *IN* directory!
-		echo "Cleaning build files..."
-		rm -f *synctex*
-		latexmk -c &> /dev/null
-		;;
-	-h)
-		help_msg
-		;;
-	*)
-		echo "Unknown course: $1... exiting"
-esac
+ep () {
+	case $1 in
+		ma)
+			set_course ~/Projects/Live/notes/snippets/Math55
+			cd ~/Classes/Math55A/HW/
+			;;
+		st)
+			set_course ~/Projects/Live/notes/snippets/Stat110
+			cd ~/Classes/Stat110/HW/
+			;;
+		ec)
+			set_course ~/Projects/Live/notes/snippets/Econ1011
+			cd ~/Classes/Econ1011A/HW/
+			;;
+		rm)
+			rm_course
+			;;
+		clean)
+			# Run *IN* directory!
+			echo "Cleaning build files..."
+			rm -f *synctex*
+			latexmk -c &> /dev/null
+			;;
+		-h)
+			help_msg
+			;;
+		*)
+			echo "Unknown course: $1... exiting"
+	esac
+}
